5dc7820ce99281f179dbaf05a33a07f165857e1d
Gawein Le Goff <gawein.le@live.fr>
2021-11-30 18:54:36 +0100

    init (all) : create project skeleton

---
+++ Source/CombatPsycasts/Bootstrap.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace CombatPsycasts
+{
+    [StaticConstructorOnStartup]
+    public static class Bootstrap
+    {
+        static Bootstrap()
+        {
+            Log.Message("[CombatPsycasts] Done intialization");
+        }    
+    }
+}
