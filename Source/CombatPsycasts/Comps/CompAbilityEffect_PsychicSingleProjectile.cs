ade9a392391f2684cda8d8d3db1e1a740888e1cd
Gawein Le Goff <gawein.le@live.fr>
2021-12-03 20:37:13 +0100

    feat (psycasts) : added psychic grenade

---
+++ Source/CombatPsycasts/Comps/CompAbilityEffect_PsychicSingleProjectile.cs
@@ -0,0 +1,37 @@
+using CombatPsycasts.DefOfs;
+using RimWorld;
+using Verse;
+
+namespace CombatPsycasts.Comps
+{
+    public class CompProperties_AbilityPsychicSingleProjectile : CompProperties_AbilityEffect
+    {
+        public ThingDef projectileDef = ProjectileDefOf.CP_Proj_PsychicGrenade;
+        
+        public CompProperties_AbilityPsychicSingleProjectile()
+        {
+            this.compClass = typeof(CompAbilityEffect_PsychicSingleProjectile);
+        }
+    }    
+    public class CompAbilityEffect_PsychicSingleProjectile : CompAbilityEffect
+    {
+        public CompProperties_AbilityPsychicSingleProjectile Props =>
+            (CompProperties_AbilityPsychicSingleProjectile)this.props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            if (target != null && this.parent.pawn is Pawn caster)
+            {
+                bool shootLineFromTo =
+                    this.parent.verb.TryFindShootLineFromTo(caster.Position, target, out ShootLine resultingLine);
+                if (!shootLineFromTo) return;
+                Projectile projectile = (Projectile)GenSpawn.Spawn(Props.projectileDef, resultingLine.Source, caster.Map);
+                ProjectileHitFlags hitFlags = ProjectileHitFlags.All;
+
+                projectile.Launch(caster, caster.DrawPos, target, target, hitFlags);
+            }    
+
+        }
+    }
+}
